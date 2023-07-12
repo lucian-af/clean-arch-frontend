94542c949d19358131f8192904e27aec27bff5a7
Lucian AF <lucian_af@hotmail.com>
2023-07-12 19:27:10 -0300

    refactor: criação da fixtureAuthentication

--- src/data/usecases/remote-authentication.spec.ts
+++ src/data/usecases/remote-authentication.spec.ts
@@ -1,11 +1,25 @@
 import { RemoteAuthentication } from './remote-authentication';
 import { HttpPostClientSpy } from '../test/mock-http-client';
 
+type FixtureTypes = {
+	httpPostClient: HttpPostClientSpy;
+	remoteAuthentication: RemoteAuthentication;
+}
+
+const fixtureAuthentication = (url = 'any'): FixtureTypes => {	
+	const httpPostClient = new HttpPostClientSpy();
+	const remoteAuthentication = new RemoteAuthentication(url, httpPostClient);
+
+	return {		
+		httpPostClient,
+		remoteAuthentication
+	};
+};
+
 describe('RemoteAuthentication', () => {
 	test('Deve chamar client HTTP com url correta ', async () => {	
 		const url = 'qualquer/url';
-		const httpPostClient = new HttpPostClientSpy();
-		const remoteAuthentication = new RemoteAuthentication(url, httpPostClient);
+		const { httpPostClient, remoteAuthentication } = fixtureAuthentication(url);
 		remoteAuthentication.auth();
 		expect(httpPostClient.url).toBe(url);
 	});
