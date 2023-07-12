6be62251cf02221ead238903229073d43bf30adf
Lucian AF <lucian_af@hotmail.com>
2023-07-12 19:20:14 -0300

    refactor: movido HttpPostClientSpy para novo arquivo

--- src/data/usecases/remote-authentication.spec.ts
+++ src/data/usecases/remote-authentication.spec.ts
@@ -1,18 +1,8 @@
-import { IHttpPostClient } from 'data/protocols/http/http-post-client';
 import { RemoteAuthentication } from './remote-authentication';
+import { HttpPostClientSpy } from '../test/mock-http-client';
 
 describe('RemoteAuthentication', () => {
-	test('Deve chamar client HTTP com url correta ', async () => {
-		class HttpPostClientSpy implements IHttpPostClient {
-			url?:string;
-
-			async post(url: string): Promise<void> {
-				this.url = url;
-				return Promise.resolve();
-			}
-
-		}
-
+	test('Deve chamar client HTTP com url correta ', async () => {	
 		const url = 'qualquer/url';
 		const httpPostClient = new HttpPostClientSpy();
 		const remoteAuthentication = new RemoteAuthentication(url, httpPostClient);
