13877a182a7d9805b522a2344530f0262459152b
Lucian AF <lucian_af@hotmail.com>
2023-07-12 19:37:46 -0300

    refactor: alteração da url para uso da lib faker

--- src/data/usecases/remote-authentication.spec.ts
+++ src/data/usecases/remote-authentication.spec.ts
@@ -1,12 +1,13 @@
 import { RemoteAuthentication } from './remote-authentication';
 import { HttpPostClientSpy } from '../test/mock-http-client';
+import { faker } from '@faker-js/faker';
 
 type FixtureTypes = {
 	httpPostClient: HttpPostClientSpy;
 	remoteAuthentication: RemoteAuthentication;
 }
 
-const fixtureAuthentication = (url = 'any'): FixtureTypes => {	
+const fixtureAuthentication = (url: string = faker.internet.url()): FixtureTypes => {	
 	const httpPostClient = new HttpPostClientSpy();
 	const remoteAuthentication = new RemoteAuthentication(url, httpPostClient);
 
@@ -18,7 +19,7 @@ const fixtureAuthentication = (url = 'any'): FixtureTypes => {
 
 describe('RemoteAuthentication', () => {
 	test('Deve chamar client HTTP com url correta ', async () => {	
-		const url = 'qualquer/url';
+		const url = faker.internet.url();
 		const { httpPostClient, remoteAuthentication } = fixtureAuthentication(url);
 		remoteAuthentication.auth();
 		expect(httpPostClient.url).toBe(url);
