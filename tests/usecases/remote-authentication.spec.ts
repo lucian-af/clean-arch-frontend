a3805966dad7e25397c9622426ea5be4fde2e0e3
Lucian AF <lucian_af@hotmail.com>
2023-07-17 21:52:31 -0300

    test: adicionado tratamento de erro 401

--- tests/usecases/remote-authentication.spec.ts
+++ tests/usecases/remote-authentication.spec.ts
@@ -2,6 +2,8 @@ import { RemoteAuthentication } from '@data/usecases/remote-authentication';
 import { faker } from '@faker-js/faker';
 import { mockAuthentication } from '@domain/test/mock-authentication';
 import { HttpPostClientSpy } from '@tests/data/mocks/mock-http-client';
+import { HttpStatusCode } from '@data/protocols/http/http-response';
+import { InvalidCredentialsError } from '@domain/errors/invalid-credentials-error';
 
 type FixtureTypes = {
 	httpPostClient: HttpPostClientSpy;
@@ -22,14 +24,25 @@ describe('RemoteAuthentication', () => {
 	test('Deve chamar HttpPostClient com url correta', async () => {	
 		const url = faker.internet.url();
 		const { httpPostClient, remoteAuthentication } = fixtureAuthentication(url);
-		remoteAuthentication.auth(mockAuthentication());
+		await remoteAuthentication.auth(mockAuthentication());
 		expect(httpPostClient.url).toBe(url);
 	});
 	
 	test('Deve chamar HttpPostClient com body correto', async () => {			
 		const { httpPostClient, remoteAuthentication } = fixtureAuthentication();
 		const params = mockAuthentication();
-		remoteAuthentication.auth(params);
+		await remoteAuthentication.auth(params);
 		expect(httpPostClient.body).toEqual(params);
+	});
+	
+	test('Deve retornar "InvalidCredentialError" se HttpPostClient retornar statusCode 401 ', async () => {			
+		const { httpPostClient, remoteAuthentication } = fixtureAuthentication();
+		httpPostClient.response = {
+			statusCode: HttpStatusCode.unauthorized
+		};
+		const params = mockAuthentication();
+		const request = remoteAuthentication.auth(params);
+		await expect(request).rejects.toThrow(new InvalidCredentialsError());
+		await expect(request).rejects.toThrow('Credenciais inválidas.');
 	});
 });
