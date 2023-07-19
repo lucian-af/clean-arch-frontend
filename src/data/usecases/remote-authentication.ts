f57783ed618a8ec65c340f38338e3b3835fa4728
Lucian AF <lucian_af@hotmail.com>
2023-07-18 21:08:50 -0300

    refactor: adequação na implementação do RemoteAuthentication

--- src/data/usecases/remote-authentication.ts
+++ src/data/usecases/remote-authentication.ts
@@ -3,20 +3,21 @@ import { HttpStatusCode } from '@data/protocols/http/http-response';
 import { InvalidCredentialsError } from '@domain/errors/invalid-credentials-error';
 import { UnexpectedError } from '@domain/errors/unexpected-error';
 import { AccountModel } from '@domain/models/account-model';
+import { IAuthentication } from '@domain/usecases/authentication';
 import { AuthenticationParams } from '@domain/usecases/authentication';
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements IAuthentication {
 	constructor(private readonly url: string, private readonly httpClient: IHttpPostClient<AuthenticationParams, AccountModel>){}
 
-	async auth(params: AuthenticationParams): Promise<void>{
+	async auth(params: AuthenticationParams): Promise<AccountModel>{
 		const httpResponse = await this.httpClient.post({
 			url: this.url,
 			body: params
 		});		
 
 		switch (httpResponse.statusCode) {
-		case HttpStatusCode.ok: 
-			break;
+		case HttpStatusCode.ok:
+			return httpResponse.body;
 		case HttpStatusCode.unauthorized:
 			throw new InvalidCredentialsError();				
 		default:
